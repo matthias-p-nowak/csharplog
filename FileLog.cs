056adc9b15a4069b24918b65ce8c2795cdc822b2
Matthias (Glorius) Genie <matthias.glorius.genie@outlook.com>
2022-10-29 19:14:52 +0200

    initial upload after getting it working

---
+++ FileLog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace csharplog
+{
+  internal class FileLog
+  {
+    private static Timer changeFileTimer = new Timer(ChangeFile);
+    private static object logLock = new object();
+    private static StreamWriter logFile = null;
+    private static void ChangeFile(object state)
+    {
+      try
+      {
+        lock (logLock)
+        {
+          logFile?.Dispose();
+          var dnstr = DateTime.Now.ToString("yyyy-MM-dd_HH");
+          var proc = Process.GetCurrentProcess();
+          var logFN = $"C:\\temp\\debug\\{proc.ProcessName}-{dnstr}-{proc.Id}-log.txt";
+          var dir = Path.GetDirectoryName(logFN);
+          Directory.CreateDirectory(dir);
+          logFile = new StreamWriter(logFN);
+          logFile.AutoFlush = true;
+        }
+      }
+      finally
+      {
+        var n = DateTime.Now;
+        var nn = new DateTime(n.Year, n.Month, n.Day, n.Hour, 0, 0);
+        nn = nn.AddHours(1);
+        var wd = (nn - n).TotalMilliseconds;
+        if (wd <= 0) wd = 10;
+        var w = (int)wd;
+        changeFileTimer.Change(w, 3600000);
+      }
+    }
+
+    static FileLog()
+    {
+      ChangeFile(null);
+      Console.WriteLine("FileLog initialized");
+    }
+    internal static void Write(string message)
+    {
+      var dts = DateTime.Now.ToString("HH:mm:ss.ffff");
+      var sf = new StackFrame(1, true);
+      var fn = Path.GetFileName(sf.GetFileName());
+      var m = sf.GetMethod();
+      var cn = m.DeclaringType.Name;
+      var ct = Thread.CurrentThread;
+      
+      lock (logLock)
+      {
+        logFile.WriteLine($"{dts} {fn}:{sf.GetFileLineNumber()}: [{cn}.{m.Name}]({ct.ManagedThreadId}) {message}");
+        Console.WriteLine($"{dts} {fn}:{sf.GetFileLineNumber()}: [{cn}.{m.Name}]({ct.ManagedThreadId}) {message}");
+      }
+    }
+  }
+}
---
+++ LogTester.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace csharplog
+{
+  class LogTester
+  {
+    private static Timer timer;
+
+    static LogTester()
+    {
+      Console.WriteLine("LogTester initialized");
+      FileLog.Write("initialized");
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Main");
+      var sw = new Stopwatch();
+      sw.Start();
+      for(int i=0;i<10;++i)
+      FileLog.Write("hello");
+      sw.Stop();
+      Console.WriteLine($"got {sw.ElapsedMilliseconds} ms");
+      var n = DateTime.Now;
+      var n1 = new DateTime(n.Year, n.Month, n.Day, n.Hour, 0, 0);
+      n1 = n1.AddHours(1);
+      var td = n1 - n;
+      Console.WriteLine($"n1= {n1:HH:mm:ss} in {td.TotalMilliseconds} ms");
+      timer = new Timer(WriteTimer,null,100,2000);
+      Console.ReadKey();
+    }
+
+    private static void WriteTimer(object state)
+    {
+      FileLog.Write($"timer {state} {Thread.CurrentThread.Name}");
+      var n = DateTime.Now;
+      var nn = new DateTime(n.Year, n.Month, n.Day, n.Hour, n.Minute, 0);
+      nn=nn.AddMinutes(1);
+      var wd =(nn - n).TotalMilliseconds;
+      if (wd <= 0) wd = 60000;
+      var w = (int)wd;
+      timer.Change(w, 2000);
+    }
+  }
+}
